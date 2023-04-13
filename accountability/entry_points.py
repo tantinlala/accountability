07da255de15c7580bf59074d880798f7bd0a17d1
Nicholas Tantisujjatham <nicholas.tantisujjatham@gmail.com>
2023-04-13 19:19:52 -0400

    Changed key to OPENAI_API_KEY

--- accountability/entry_points.py
+++ accountability/entry_points.py
@@ -5,4 +5,4 @@ def run():
     AUTHENTICATION_FILE = 'auth.yaml'
     with open(AUTHENTICATION_FILE, 'r') as auth_file:
         auth_dict = yaml.safe_load(auth_file)
-        print(auth_dict['openai']['access_token'])
+        print(auth_dict['openai']['OPENAI_API_KEY'])
