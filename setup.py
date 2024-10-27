925128db2465d859fc608247b08da3c769a6c617
tantinlala <nicholas.tantisujjatham@gmail.com>
2024-10-27 17:21:23 -0400

    Remove summarize diffs console script

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
             "accountability_get_amendment = accountability.entry_points:get_amendment",
             "accountability_summarize = accountability.entry_points:summarize",
             "accountability_diff_with_previous = accountability.entry_points:diff_with_previous",
-            "accountability_summarize_diffs = accountability.entry_points:summarize_diffs",
             "accountability_process_hr_rollcalls = accountability.entry_points:process_hr_rollcalls"
         ]
     },
